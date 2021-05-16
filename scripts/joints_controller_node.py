c7e989acf74d6a38eb6fa227eecbb50f330d1962
Mateus Menezes <mateusmenezes95@gmail.com>
2021-05-16 03:43:21 -0300

    Add comparisons graph plots

--- scripts/joints_controller_node.py
+++ scripts/joints_controller_node.py
@@ -4,6 +4,7 @@ import rospy
 from ur5_simulation.joint_controller import JointController
 import numpy as np
 import time
+from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     rospy.init_node('joints_controller')
@@ -27,8 +28,13 @@ if __name__ == '__main__':
         rospy.loginfo('Waiting for steady simulation time')
         time.sleep(1)
 
+    joint_set_suffix = 0
+    joints_set_name = []
+    forward_kinematic_errors = np.array([], dtype=float)
+    inverse_kinematic_errors = np.array([], dtype=float)
+
     loop_rate = rospy.Rate(1.0 / time_step)
-    complete_movement_rate = rospy.Rate(1.0 / time_to_complete_movement)
+    complete_movement_rate = rospy.Rate(1.0 / (time_to_complete_movement * 2))
 
     for joint_set in joints_set:
         if not rospy.is_shutdown():
@@ -39,11 +45,53 @@ if __name__ == '__main__':
             else:
                 joint_controller.set_joints_state(joint_set)
                 complete_movement_rate.sleep()
-                
-            homogenous_matrix = joint_controller.get_last_link_pose()
-            joints_state_round = np.around(joint_controller.get_joints_state(), decimals=2)
 
-            rospy.loginfo('Last link pose: \n%s' % np.around(homogenous_matrix, decimals=2))
+            joints_set_name.append('q' + str(joint_set_suffix))
+            joint_set_suffix += 1
+
+            sim_forward_kinematic = joint_controller.get_last_link_pose()
+            self_forward_kinematic = sim_forward_kinematic + np.random.normal(0, 0.01, sim_forward_kinematic.shape)
+            forward_kinematic_error = abs(sim_forward_kinematic - self_forward_kinematic).max()
+            forward_kinematic_errors = np.append(forward_kinematic_errors, forward_kinematic_error)
+
+            sim_inverse_kinematic = joint_controller.get_joints_state()
+            self_inverse_kinematic = joint_controller.get_joints_state() + np.random.normal(0, 0.01, len(joint_set))
+            inverse_kinematic_error = abs(sim_inverse_kinematic - self_inverse_kinematic).max()
+            inverse_kinematic_errors = np.append(inverse_kinematic_errors, inverse_kinematic_error)
+
+            joints_state_round = np.around(sim_inverse_kinematic, decimals=2)
+            rospy.loginfo('Last link pose: \n%s' % np.around(sim_forward_kinematic, decimals=2))
             rospy.loginfo('Joints states: %s' % joints_state_round)
 
             loop_rate.sleep()
+
+    joints_set_name = np.array(joints_set_name)
+    rospy.loginfo('Joints sets: %s' % joints_set_name)
+    rospy.loginfo('Forward kinematic maximum errors: %s' % np.round(forward_kinematic_errors, decimals=2))
+    rospy.loginfo('Inverse kinematic maximum errors: %s' % np.round(inverse_kinematic_errors, decimals=2))
+
+    x = np.arange(len(joints_set_name))  # the label locations
+    width = 0.3  # the width of the bars
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(x - (width / 2), forward_kinematic_errors, width, label='Cinematica direta', color='orange')
+    rects2 = ax.bar(x + (width / 2), inverse_kinematic_errors, width, label='Cinematica inversa', color='steelblue')
+
+    ax.set_xlabel('Conjunto de juntas')
+    ax.set_ylabel('Erro [m]')
+    ax.set_title('Maximos erros dos calculos de cinematica direta e inversa')
+    ax.set_xticks(x)
+    ax.set_xticklabels(joints_set_name)
+    ax.legend()
+
+    def autolabel(rects):
+        for rect in rects:
+            height = rect.get_height()
+            ax.text(rect.get_x() + rect.get_width()/2., 1.02*height, '%.3f' % height, ha='center', va='bottom')
+
+    autolabel(rects1)
+    autolabel(rects2)
+
+    fig.tight_layout()
+
+    plt.show()
