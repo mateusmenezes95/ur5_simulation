630dcc20897aacc9a3f28b2e4ff4b99e71e427fc
Mateus Menezes <mateusmenezes95@gmail.com>
2021-05-27 21:09:03 -0300

    Add translation and joints errors chart and fix (truly) time sync bug

--- scripts/joints_controller_node.py
+++ scripts/joints_controller_node.py
@@ -23,16 +23,24 @@ if __name__ == '__main__':
         rospy.loginfo('Waiting for simulation time to be non-zero')
         time.sleep(0.1)
 
-    joint_set_suffix = 0
     joints_set_name = []
-    forward_kinematic_errors = np.array([], dtype=float)
-    inverse_kinematic_errors = np.array([], dtype=float)
+    forward_kinematic_max_errors = np.array([], dtype=float)
+    inverse_kinematic_max_errors = np.array([], dtype=float)
+    inverse_kinematic_errors = np.zeros(shape=(len(joints_set), len(joints_set[0])))
+    forward_kinematic_translation_errors = np.zeros(shape=(len(joints_set), 3))
 
     loop_rate = rospy.Rate(1.0 / time_step)
 
-    for joint_set in joints_set:
+    previous_time = rospy.get_time()
+    actual_time = previous_time
+    while actual_time < previous_time + 1:
+        actual_time = rospy.get_time()
+        rospy.loginfo('Waiting for steady simulation time')
+        time.sleep(1)
+
+    for joint_set_index, joint_set in enumerate(joints_set):
         if not rospy.is_shutdown():
-            set_name = 'q' + str(joint_set_suffix)
+            set_name = 'q' + str(joint_set_index)
             rospy.loginfo('Values of %s joint set sent to UR5 controller: %s' % (set_name, joint_set))
 
             if max(abs(joint_set - joint_controller.get_joints_state())) < 0.05:    # avoid unnecessary movement
@@ -43,32 +51,51 @@ if __name__ == '__main__':
                 rospy.sleep(time_to_complete_movement * 1.5)
 
             joints_set_name.append(set_name)
-            joint_set_suffix += 1
 
             sim_forward_kinematic = joint_controller.get_last_link_pose()
             self_forward_kinematic = ur5_kinematics.get_forward_kinematics(joint_set, short=False)
-            forward_kinematic_error = abs(sim_forward_kinematic - self_forward_kinematic).max()
-            forward_kinematic_errors = np.append(forward_kinematic_errors, forward_kinematic_error)
+            forward_kinematic_max_error = abs(sim_forward_kinematic - self_forward_kinematic).max()
+            forward_kinematic_max_errors = np.append(forward_kinematic_max_errors, forward_kinematic_max_error)
+            forward_kinematic_translation_error = (sim_forward_kinematic[0:3, 3] - self_forward_kinematic[0:3, 3]).T
+            forward_kinematic_translation_errors[joint_set_index] = forward_kinematic_translation_error
 
             sim_inverse_kinematic = joint_controller.get_joints_state()
             self_inverse_kinematic = joint_controller.get_joints_state() + np.random.normal(0, 0.01, len(joint_set))
-            inverse_kinematic_error = abs(sim_inverse_kinematic - self_inverse_kinematic).max()
-            inverse_kinematic_errors = np.append(inverse_kinematic_errors, inverse_kinematic_error)
+            inverse_kinematic_error = sim_inverse_kinematic - self_inverse_kinematic
+            inverse_kinematic_errors[joint_set_index] = inverse_kinematic_error
+            inverse_kinematic_max_error = abs(inverse_kinematic_error).max()
+            inverse_kinematic_max_errors = np.append(inverse_kinematic_max_errors, inverse_kinematic_max_error)
 
-            rospy.debug('Last link pose from ROS: \n%s' % np.around(sim_forward_kinematic, decimals=2))
-            rospy.debug('Last link pose from self computation: \n%s' % np.around(self_forward_kinematic, decimals=2))
-            rospy.debug('Joints values from ROS: %s' % np.around(sim_inverse_kinematic, decimals=2))
-            rospy.debug('Joints values from Self computation: %s' % np.around(self_inverse_kinematic, decimals=2))
+            rospy.logdebug('Joints errors of %s set: %s' % (set_name, inverse_kinematic_error))
+            rospy.logdebug('Last link pose from ROS: \n%s' % np.around(sim_forward_kinematic, decimals=2))
+            rospy.logdebug('Last link pose from self computation: \n%s' % np.around(self_forward_kinematic, decimals=2))
+            rospy.logdebug('Joints values from ROS: %s' % np.around(sim_inverse_kinematic, decimals=2))
+            rospy.logdebug('Joints values from Self computation: %s' % np.around(self_inverse_kinematic, decimals=2))
 
             loop_rate.sleep()
 
     joints_set_name = np.array(joints_set_name)
     rospy.loginfo('Joints sets: %s' % joints_set_name)
-    rospy.loginfo('Forward kinematic maximum errors: %s' % np.round(forward_kinematic_errors, decimals=2))
-    rospy.loginfo('Inverse kinematic maximum errors: %s' % np.round(inverse_kinematic_errors, decimals=2))
+    rospy.loginfo('Tranlation x errors %s' % np.round(forward_kinematic_translation_errors[:,0], decimals=5))
+    rospy.loginfo('Tranlation y errors %s' % np.round(forward_kinematic_translation_errors[:,1], decimals=5))
+    rospy.loginfo('Tranlation z errors %s' % np.round(forward_kinematic_translation_errors[:,2], decimals=5))
+    rospy.loginfo('Forward kinematic maximum errors: %s' % np.round(forward_kinematic_max_errors, decimals=2))
+    rospy.loginfo('Inverse kinematic maximum errors: %s' % np.round(inverse_kinematic_max_errors, decimals=2))
 
     cg.generate_diference_between_kinematics_computation(
         joints_set_name,
-        forward_kinematic_errors,
+        forward_kinematic_max_errors,
+        inverse_kinematic_max_errors
+    )
+
+    cg.generate_translation_errors(
+        joints_set_name,
+        forward_kinematic_translation_errors
+    )
+
+    cg.generate_joints_errors(
+        joints_set_name,
         inverse_kinematic_errors
     )
+
+    cg.show_charts()
--- src/ur5_simulation/chart_generation.py
+++ src/ur5_simulation/chart_generation.py
@@ -4,7 +4,7 @@ import numpy as np
 def autolabel(ax, rects):
     for rect in rects:
         height = rect.get_height()
-        ax.text(rect.get_x() + rect.get_width() / 2., (1.02 * height), '%.3f' % height, ha='center', va='bottom')
+        ax.text(rect.get_x() + rect.get_width() / 2., (1.02 * height), '%.5f' % height, ha='center', va='bottom')
 
 def generate_diference_between_kinematics_computation(x_labels, forward_kinematic, inverse_kinematic):
     x = np.arange(len(x_labels))  # the label locations
@@ -14,11 +14,11 @@ def generate_diference_between_kinematics_computation(x_labels, forward_kinemati
     rects1 = ax.bar(x - (bar_width / 2), forward_kinematic, bar_width, label='Cinematica direta', color='orange')
     rects2 = ax.bar(x + (bar_width / 2), inverse_kinematic, bar_width, label='Cinematica inversa', color='steelblue')
 
-    ax.set_xlabel('Conjunto de juntas')
-    ax.set_ylabel('Erro [m]')
     ax.set_title('Maximos erros dos calculos de cinematica direta e inversa')
     ax.set_xticks(x)
     ax.set_xticklabels(x_labels)
+    ax.set_xlabel('Conjunto de valores de juntas')
+    ax.set_ylabel('Erro [m ou rad]')
     ax.legend()
 
     autolabel(ax, rects1)
@@ -26,4 +26,41 @@ def generate_diference_between_kinematics_computation(x_labels, forward_kinemati
 
     fig.tight_layout()
 
+def generate_translation_errors(x_labels, translation_errors):
+    x = np.arange(len(x_labels))  # the label locations
+    
+    fig, ax = plt.subplots()
+
+    ax.plot(x_labels, translation_errors, marker='o')
+
+    ax.set_title('Erros de translacao')
+    ax.set_xticks(x)
+    ax.set_xticklabels(x_labels)
+    ax.set_xlabel('Conjunto de valores de juntas')
+    ax.set_ylabel('Erro [m]')
+    ax.legend(['Eixo X', 'Eixo Y', 'Eixo Z'])
+    ax.grid(alpha=0.2)
+    # ax.set_xlim([0, len(x) - 1])
+    
+    fig.tight_layout()
+
+def generate_joints_errors(x_labels, joints_errors):
+    x = np.arange(len(x_labels))  # the label locations
+    
+    fig, ax = plt.subplots()
+
+    ax.plot(x_labels, joints_errors, marker='o')
+    
+    ax.set_title('Erros do angulo das juntas')
+    ax.set_xticks(x)
+    ax.set_xticklabels(x_labels)
+    ax.set_xlabel('Conjunto de valores de juntas')
+    ax.set_ylabel('Erro [rad]')
+    ax.legend(['junta 1', 'junta 2', 'junta 3', 'junta 4', 'junta 5', 'junta 6'])
+    ax.grid(alpha=0.2)
+    # ax.set_xlim([0, len(x) - 1])
+    
+    fig.tight_layout()
+
+def show_charts():
     plt.show()
